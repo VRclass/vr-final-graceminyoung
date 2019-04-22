625887b89fe023f712890a21134fc9623e360318
GraceMint <46830892+graceminyoung@users.noreply.github.com>
2019-04-22 18:22:14 -0500

    Final Project
    Project1_Final

---
+++ Assets/SAIC-VRclass/Scripts/TriggerLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLight : MonoBehaviour {
+	public Light ligthToSwitch = null;
+
+	public void OnTriggerEnter(Collider other) {
+		ligthToSwitch.enabled = false ;
+		print ("entered the trigger");
+	}
+
+	public void OnTriggerExit(Collider other){
+		ligthToSwitch.enabled = true;
+	}
+}
---
+++ Assets/SAIC-VRclass/Scripts/TriggerSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(AudioSource))]
+
+public class TriggerSound : MonoBehaviour {
+	private AudioSource audio;
+	public AudioClip clip;
+
+	void Awake(){
+		audio = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		//This will play without any testing
+//		audio.Play();
+
+		//This will play only if the player collides with the trigger
+		if (other.tag == "Player"){
+			audio.Play ();
+			Debug.Log("trigger entered & played sound: "+clip);
+		}
+	}
+}
